eaf5b46da8886c101da1924b40a6d15f5b93586a
kurtjang <kurtjang@gmail.com>
2026-03-27 02:09:40 +0900

    Quartz sync: Mar 27, 2026, 2:09 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "jsukjin.github.io/KurtBlog",
+    baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
